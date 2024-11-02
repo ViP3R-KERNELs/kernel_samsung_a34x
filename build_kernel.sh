e1854f71b43e88aa76d80a58ef8dd4dd1d52a0c0
IamCOD3X <sourabh.panchal28@gmail.com>
2024-11-02 11:48:13 +0530

    Build: setting up

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin:$PATH
-export CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin/aarch64-linux-gnu-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin/clang
+export PATH=$(pwd)/clang/bin:$PATH
+export CROSS_COMPILE=$(pwd)/clang/bin/aarch64-linux-gnu-
+export CC=$(pwd)/clang/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
-export PLATFORM_VERSION=14
-export TARGET_SOC=mt6877
-export TARGET_BUILD_VARIANT=user
+export PLATFORM_VERSION=12
+export LOCALVERSION=-ViP3R🐍-v1.0-NETHUNTER-RELOADED
+export KBUILD_BUILD_USER=IamCOD3X
+export KBUILD_BUILD_HOST=RYZEN
+export DTC_EXT=/usr/bin/dtc
 
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 a34x_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 -j16
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 -j8
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
